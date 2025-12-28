85af82fb9599d18bc2778c6f908ca7e91448067e
nguyenbabinh2701 <babinh511@gmail.com>
2025-12-29 06:35:49 +0700

    task16

---
+++ task16/ex01.js
@@ -0,0 +1,13 @@
+function delayLog(message, ms) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(message);
+            resolve();
+        }, ms);
+    });
+}
+
+
+delayLog("Hello", 1000).then(() => {
+    console.log("Done");
+});
